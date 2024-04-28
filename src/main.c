720b6b1b95f4037bb36b84bda785e4e3f5181ae8
Kheireddine <kheireddine.zaillel.1@ens.etsmtl.ca>
2024-04-28 14:57:34 +0200

    Init project (Nrf52840 Dongle with toggled led)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+
+
+#define MY_GPIO0 DT_NODELABEL(gpio0)
+#define LED_PIN 6
+
+const struct device *gpio0_dev = DEVICE_DT_GET(MY_GPIO0);
 
 int main(void)
 {
-        return 0;
+
+  gpio_pin_configure(gpio0_dev, LED_PIN, GPIO_OUTPUT);
+
+  while (1)
+  {
+    gpio_pin_toggle(gpio0_dev, LED_PIN);
+    k_msleep(1000);
+  }
 }
